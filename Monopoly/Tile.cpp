8c30751960cef2d54d0f2a98d4984ffe8be3a5db
pustowoit.maxim <pustowoit.maxim@mail.ru>
2022-09-15 19:16:55 +0300

    15.09.22

---
+++ Monopoly/Tile.cpp
@@ -0,0 +1,39 @@
+#include "Tile.h"
+/*enum class TileType {
+    Start,
+    Railroad,
+    Commercial_services,
+    Property,
+    Chance,
+    Community_Chest,
+    Tax,
+    Prison,
+};
+enum class Property_rank {
+    Browm,
+    Blue,
+    Purple,
+    Red,
+    Green,
+    Orange,
+    Yellow,
+    Sky_blue,
+};
+
+class Tile {
+};
+class Property : Tile {
+private:
+    int cost;
+    //Property_rank rank;
+    int house_price;
+public:
+    int set_price(Property_rank rank) {
+        cost = static_cast<int>(rank);
+    }
+
+};
+*/
+Tile::Tile() {
+
+}
